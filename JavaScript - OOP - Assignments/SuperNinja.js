69b46dbae4bf215839f067365db2a0c955c3e0ed
Loai A. Zaidan <loai.zaidan@axsos.academy>
2025-02-01 16:58:36 +0200

    adding assignments

---
+++ JavaScript - OOP - Assignments/SuperNinja.js
@@ -0,0 +1,46 @@
+class Ninja {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName() {
+        console.log(`Ninja's Name: ${this.name}`);
+    }
+
+    showStats() {
+        console.log(`Name: ${this.name}, Strength: ${this.strength}, Speed: ${this.speed}, Health: ${this.health}`);
+    }
+
+    drinkSake() {
+        this.health += 10;
+        console.log(`${this.name} drank sake and gained 10 health!`);
+    }
+}
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom() {
+        this.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
